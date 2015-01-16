30c88705ee4e0f575f7d927a5d7a3e025c1b5ead
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2015-01-16 17:39:36 +0000

    Monitor the web learn pull requests.

--- jobs/github_prs.rb
+++ jobs/github_prs.rb
@@ -1,21 +1,7 @@
 require 'octokit'
 
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  client = Octokit::Client.new(:access_token => "YOUR_GITHUB_AUTH_TOKEN")
-  my_organization = "YOUR_EXACT_GITHUB_ORGANIZATION_NAME"
-  repos = client.organization_repositories(my_organization).map { |repo| repo.name }
-
-  open_pull_requests = repos.inject([]) { |pulls, repo|
-    client.pull_requests("#{my_organization}/#{repo}", :state => 'open').each do |pull|
-      pulls.push({
-        title: pull.title,
-        repo: repo,
-        updated_at: pull.updated_at.strftime("%b %-d %Y, %l:%m %p"),
-        creator: "@" + pull.user.login,
-        })
-    end
-    pulls
-  }
-
-  send_event('PR_WIDGET_DATA_ID', { header: "Open Pull Requests", pulls: open_pull_requests })
+  client = Octokit::Client.new()
+  repos = client.search_issues("user:ox-it wl- is:open type:pr")
+  send_event("pull.requests", { current: repos.total_count })
 end
